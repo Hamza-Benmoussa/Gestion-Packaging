5d97da84240e4e45d41b40c463d4cf53d49dac21
Hamza Benmoussa <hamzabenmoussa166@gmail.com>
2023-04-10 18:13:00 +0000

    init

---
+++ src/main/java/com/leoni/packaging/GestionPackageApplication.java
@@ -0,0 +1,13 @@
+package com.leoni.packaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPackageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoni/packaging/GestionPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoni.packaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
