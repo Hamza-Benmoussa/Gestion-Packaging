b31f78992a48ac0aefff9318f59ea30d5e76dfd7
Hamza Benmoussa <hamzabenmoussa166@gmail.com>
2023-05-07 12:35:27 +0100

    add mvc ErrorController

---
+++ src/main/java/com/leoni/packaging/web/CustomeErrorController.java
@@ -0,0 +1,32 @@
+package com.leoni.packaging.web;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomeErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "errorPages/404";
+            }else if(statusCode == HttpStatus.FORBIDDEN.value()){
+                return "errorPages/403";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "errorPages/500";
+            }
+        }
+        return "errorPages/error";
+    }
+
+}
